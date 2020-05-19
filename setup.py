2a820c25c0d5accde3540e40814fde276697b115
Iwan Trofimtschuk <iwan@djangsters.de>
2020-05-19 15:22:30 +0200

    Align python version classifiers in setup.py with tested versions

--- setup.py
+++ setup.py
@@ -42,5 +42,7 @@ setup(
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ]
 )
